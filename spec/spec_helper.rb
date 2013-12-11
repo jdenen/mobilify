c79f2248e1ad37097ec179e1ccc649e80834b1af
Johnson Denen <jdenen@manta.com>
2013-12-10 22:07:47 -0500

    create spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'rspec-given'
+require 'watir-webdriver'
+require 'webdriver-user-agent'
+
+$:.unshift(File.dirname(File.expand_path('lib')))
+$:.unshift(File.dirname(File.expand_path('spec/support')))
+
+RSpec.configure do |config|
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run_excluding :skip => true
+  config.color_enabled = true
+
+end
