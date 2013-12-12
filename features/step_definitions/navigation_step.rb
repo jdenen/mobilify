2b5dda39d2db876eba6146dc5227ed64e20ca2fd
Johnson Denen <jdenen@manta.com>
2013-12-11 22:44:30 -0500

    add definitions

---
+++ features/step_definitions/navigation_step.rb
@@ -0,0 +1,14 @@
+Given /^I am using Firefox$/ do
+  @browser = Watir::Browser.new
+  @agent = :desktop
+end
+
+Given /^I am using a mobile device$/ do
+  driver = Webdriver::UserAgent.driver(:browser => :firefox, :agent => :iphone)
+  @browser = Watir::Browser.new driver
+  @agent = :mobile
+end
+
+Given /^I navigate to a Manta profile$/ do
+  @page = Manta.new(@browser, :agent => @agent, :visit => true)
+end
