fb361c5a57c3a02e9b9244c2de66f0bc90cb027a
Johnson Denen <jdenen@manta.com>
2013-12-11 22:43:32 -0500

    add test support

---
+++ features/support/env.rb
@@ -0,0 +1,6 @@
+require 'rspec'
+require 'rspec-given'
+require 'watir-webdriver'
+require 'webdriver-user-agent'
+
+$:.unshift(File.dirname(File.expand_path('lib')))
---
+++ features/support/manta.rb
@@ -0,0 +1,11 @@
+require 'mobilify'
+
+class Manta
+  include Mobilify
+
+  page_url "http://www.manta.com/c/mm2fqg7"
+
+  link(:map, :id => "map-tab-link")
+  link(:mobile_map, :class => "pvm prm")
+
+end
